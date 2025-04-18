f0e2ce36c9139dc4aa9b7df3235df458e4dbef32
anuragjipo <anuujajsi@gmail.com>
2025-04-19 00:56:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,10 +78,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/23b40860573d44ecc1e77-44ebcacfc1cafba464.jpg"
+    "START_IMG_URL", "https://graph.org/file/bacd8598eb514e3d8ee09-e22513d1e973bb1905.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/23b40860573d44ecc1e77-44ebcacfc1cafba464.jpg"
+    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
